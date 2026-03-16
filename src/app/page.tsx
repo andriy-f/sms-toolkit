332aaa05e3440447caff2a3f7848078801d959da
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2026-03-16 12:03:18 +0000

    warning fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import Image from "next/image";
 import { Suspense } from "react";
 import Wizard from "../features/Wizard";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Suspense
           fallback={<div className="text-sm text-gray-500">Loading...</div>}
         >
           <Wizard />
         </Suspense>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
     </div>
   );
